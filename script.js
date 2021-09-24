1b6e18566e0c0a0f965c038bb2c486e51e4b3c78
Alex Scerba <a.scerba02@gmail.com>
2021-09-24 19:32:03 -0400

    Complete site restructure. Changed tags to better represent the situation. Added JavaScript for better nav bar

---
+++ script.js
@@ -0,0 +1,30 @@
+function stickyTopNav() {
+  var fixed_block_nav = document.getElementById("fixed-block-navigation").style;
+  if (document.body.scrollTop > 175 || document.documentElement.scrollTop > 175) {
+    fixed_block_nav.display = "inline-block";
+
+  } else {
+    fixed_block_nav.display = "none";
+  }
+};
+
+function stickySideNav() {
+  var side_nav = document.getElementById("side-navigation").style;
+  var main_w_aside = document.getElementById("main-w-aside").style;
+  if (document.body.scrollTop > 175 || document.documentElement.scrollTop > 175) {
+    side_nav.position = "fixed";
+    side_nav.left = "0";
+    side_nav.top = "79px";
+    main_w_aside.marginLeft = "17%";    
+  } else {
+    side_nav.position = "relative";
+    side_nav.left = "auto";
+    side_nav.top = "auto";
+    main_w_aside.marginLeft = "0";
+  }
+};
+
+window.onscroll = function() {
+  stickyTopNav();
+  stickySideNav();
+};
