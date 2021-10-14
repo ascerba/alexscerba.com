85d6c8203961e8086f5fcd9d66ad6f4409dbfe30
Alex Scerba <a.scerba02@gmail.com>
2021-10-14 10:16:04 -0400

    Added new way of toggling stick top nav lines #2,4-6,8-9 Checks scroll position and toggles CSS class depending on location. Added mobile nav function lines #18-22 Toggles 'active' class on press.

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 function stickyTopNav() {
-  var fixed_block_nav = document.getElementById("fixed-block-navigation").style;
+  var nav = document.querySelector('.nav');
   if (document.body.scrollTop > 194 || document.documentElement.scrollTop > 194) {
-    fixed_block_nav.display = "inline-block";
-
+    if (document.querySelector('.sticky') == null) {
+      nav.classList.toggle('sticky');
+    }
   } else {
-    fixed_block_nav.display = "none";
-  }
-};
-
-function stickySideNav() {
-  var side_nav = document.getElementById("side-navigation").style;
-  var main_w_aside = document.getElementById("main-w-aside").style;
-  if (document.body.scrollTop > 194 || document.documentElement.scrollTop > 194) {
-    side_nav.position = "fixed";
-    side_nav.left = "0";
-    side_nav.top = "79px";
-    main_w_aside.marginLeft = "17%";    
-  } else {
-    side_nav.position = "relative";
-    side_nav.left = "auto";
-    side_nav.top = "auto";
-    main_w_aside.marginLeft = "0";
+    if (document.querySelector('.sticky') != null) {
+      nav.classList.toggle('sticky');
+    }
   }
 };
 
 window.onscroll = function() {
   stickyTopNav();
 };
+
+function actionToggle() {
+  var action = document.querySelector('.action');
+  action.classList.toggle('active')
+}
